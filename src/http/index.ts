69a4f403c0d9d42b634d012a61c13ab180128a0f
Bacuuu <823727097@qq.com>
2021-06-07 23:20:54 +0800

    fix:修改http为默认contenttype为form，为post请求自动加上时间戳

--- src/http/index.ts
+++ src/http/index.ts
@@ -5,15 +5,20 @@ type MethodType = (method: any) => IType
 function mergeParams (params) {
   const defaultParams = {
     dataType: 'json',
-    header:{},
+    header:{
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
     // cors_mode: process.env.NODE_ENV === 'development' ? '' : 'no-cors'
   }
   Object.assign(params, defaultParams)
-  return params
 }
 const genMethod:MethodType = function (method) {
   return (url, data = {}, otherParams = {}) => {
-    const _url = `http://localhost:3000${url}`
+    let _url = `http://localhost:3000${url}`
+    if (method.toUpperCase() === 'POST') {
+      const timestamp = +new Date()
+      _url+=`?timestamp=${timestamp}`
+    }
     mergeParams(otherParams)
     return Taro.request({
       url: _url,
@@ -27,15 +32,6 @@ const get = genMethod('get')
 const post = genMethod('post')
 const put = genMethod('put')
 const _delete = genMethod('delete')
-// const get: IType = (url, data = {}, otherParams = {}) => {
-//   mergeParams(otherParams)
-//   return Taro.request({
-//     url,
-//     method: 'GET',
-//     data,
-//     ...otherParams
-//   })
-// }
 export default {
   get,
   post,
