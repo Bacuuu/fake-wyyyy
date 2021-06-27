f199d3b4814e494bdcb1d2bd9afd40a62fc85e73
Bacuuu <823727097@qq.com>
2021-06-27 14:26:27 +0800

    fix(redux):修改登陆后储存的数据

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -2,8 +2,10 @@ import {
   UPDATE,
   RESET
 } from '@/constants/user'
-import { loginByPhone, getLoginStatus } from '@/api/user'
 import Taro from '@tarojs/taro'
+import { AtMessage } from 'taro-ui'
+import { loginByPhone, getUserAccount, getUserLevel, getUserMusicInfo, getLoginStatus } from '@/api/user'
+import { filterBeforeMergeResponse } from '@/util'
 
 export const update = (params) => {
   return {
@@ -17,13 +19,47 @@ export const reset = () => {
   }
 }
 
-export const login = ({ phone, password }: {phone: number, password: string}) => {
+export const login = ({ phone, password }: {phone: number | string, password: string}) => {
   return async dispatch => {
-    const res:any = await loginByPhone({
+    const res = await loginByPhone({
       phone,
       password
     })
-    res.iStatus && dispatch(update(res.data))
+    if (res.code !== 200) {
+      const err = res.message || res.msg || '登录错误'
+      Taro.atMessage({
+        message: err,
+        type: 'error'
+      })
+      throw Error(err)
+    }
+    Taro.atMessage({
+      message: '登录成功',
+      type: 'success'
+    })
+    const promiseList = [
+    getUserAccount(),
+    getUserLevel(),
+    getUserMusicInfo()]
+    const userInfo = {
+      account: null,
+      level: null,
+      musicinfo: null
+    }
+    Promise.all(promiseList)
+      .then(r => {
+        if (r.some(i => i.code !== 200)) {
+          Taro.atMessage({
+            message: '获取部分用户信息失败',
+            type: 'warning'
+          })
+        }
+        console.log(r)
+        userInfo.account = filterBeforeMergeResponse(r[0])
+        userInfo.level = filterBeforeMergeResponse(r[1])
+        userInfo.musicinfo = filterBeforeMergeResponse(r[2])
+      })
+    res.iStatus && dispatch(update(userInfo))
   }
 }
 
