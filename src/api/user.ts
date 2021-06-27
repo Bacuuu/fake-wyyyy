8b1ae798d56c7369e7fef1905d43e36a2daa5c2f
Bacuuu <823727097@qq.com>
2021-06-27 14:29:48 +0800

    feat:新增诸多用户接口

--- src/api/user.ts
+++ src/api/user.ts
@@ -3,6 +3,18 @@ export const loginByPhone = (params) => {
   return http.post('/login/cellphone', params)
 }
 
+export const getUserAccount = () => {
+  return http.post('/user/account')
+}
+
+export const getUserMusicInfo = () => {
+  return http.post('/user/subcount')
+}
+
+export const getUserLevel = () => {
+  return http.post('/user/level')
+}
+
 export const getLoginStatus = () => {
   return http.post('/login/status')
 }
