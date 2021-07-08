def45773dc312ac5e1b76480888d2627595b916d
Bacuuu <823727097@qq.com>
2021-07-08 22:04:27 +0800

    账号页面顶部信息

--- src/pages/me/me.tsx
+++ src/pages/me/me.tsx
@@ -1,9 +1,9 @@
 import Taro from '@tarojs/taro'
-import { View, Image } from '@tarojs/components'
+import { View, Image, Text } from '@tarojs/components'
 import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { IStoreType } from '@/types/store'
-import { AtButton } from 'taro-ui'
+import { AtButton, AtTag } from 'taro-ui'
 import './me.scss'
 
 type IProps = {
@@ -20,13 +20,27 @@ function Me (props: IProps) {
       url: '/pages/me/login'
     })
   }
+  function getuser () {
+    console.log(user)
+    return user?.userInfo?.account?.profile?.avatarUrl
+  }
   return (
     <View className='me-wrap'>
       {
         hasLogin 
           ? (
             <View className="has-login">
-
+              <View className="user-info">
+                <View className="user-info__top">
+                  <Image className="avatar" src={user?.userInfo?.account?.profile?.avatarUrl} />
+                  <View className="basic-info">
+                    <View>{user.userInfo.account.profile.nickname}</View>
+                    <AtTag>Lv.{user.userInfo.level.level}</AtTag>
+                  </View>
+                  <AtButton type="primary" circle={false} className="ding">签到</AtButton>
+                  {/* <AtButton onClick={getuser}>123</AtButton> */}
+                </View>
+              </View>
             </View>
           )
           : (
