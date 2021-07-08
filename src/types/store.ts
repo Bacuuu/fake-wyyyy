aed4b8f9692d01c119f03bb03dd5f201b8e62399
Bacuuu <823727097@qq.com>
2021-07-08 22:04:55 +0800

    feat:完善userinfo类型定义

--- src/types/store.ts
+++ src/types/store.ts
@@ -3,7 +3,84 @@ type IStoreType = {
 }
 
 type IStoreUserType = {
-  userInfo: object | null
+  userInfo: {
+    account: {
+      account: {
+        id: number;
+        userName: string;
+        type: number;
+        status: number;
+        whitelistAuthority: number;
+        createTime: number;
+        tokenVersion: number;
+        ban: number;
+        baoyueVersion: number;
+        donateVersion: number;
+        vipType: number;
+        anonimousUser: boolean;
+        paidFee: boolean;
+      },
+      profile: {
+        userId: number;
+        userType: number;
+        nickname: string;
+        avatarImgId: number;
+        avatarUrl: string;
+        backgroundImgId: number;
+        backgroundUrl: string;
+        signature: string;
+        createTime: number;
+        userName: string;
+        accountType: number;
+        shortUserName: string;
+        birthday: number;
+        authority: number;
+        gender: number;
+        accountStatus: number;
+        province: number;
+        city: number;
+        authStatus: number;
+        description: String;
+        detailDescription: String;
+        defaultAvatar: boolean;
+        expertTags: String;
+        experts: String;
+        djStatus: number;
+        locationStatus: number;
+        vipType: number;
+        followed: boolean;
+        mutual: boolean;
+        authenticated: boolean;
+        lastLoginTime: number;
+        lastLoginIP: string;
+        remarkName: string;
+        viptypeVersion: number;
+        authenticationTypes: number;
+        avatarDetail: string;
+        anchor: boolean;
+      }
+    },
+    level: {
+      userId: number;
+      info: string;
+      progress: number;
+      nextPlayCount: number;
+      nextLoginCount: number;
+      nowPlayCount: number;
+      nowLoginCount: number;
+      level: number;
+    },
+    musicinfo: {
+      programCount: number;
+      djRadioCount: number;
+      mvCount: number;
+      artistCount: number;
+      newProgramCount: number;
+      createDjRadioCount: number;
+      createdPlaylistCount: number;
+      subPlaylistCount: number;
+    }
+  }
 }
 export {
   IStoreType,
