96a0676f506461364544ec89deb23febf3aa8b8e
Bacuuu <823727097@qq.com>
2021-11-01 23:12:17 +0800

    feat:新增iconfont多ttf文件使用

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,34 +1,26 @@
-import { useEffect, useState } from 'react'
 import { View, Text } from '@tarojs/components'
-import { useDispatch, useSelector } from 'react-redux'
-import { login, loginStatus } from '../../actions/user'
-import { IStoreType } from '@/types/store'
+import { AtInput, AtIcon } from 'taro-ui'
 import './index.scss'
 
 type IProps = {
   user?: Object
 }
 function Index (props: IProps) {
-  // const [ user ] = useState(() => {})
-  const redux = useSelector((state: IStoreType) => state.user)
-  const dispatch = useDispatch()
-  // console.log(redux.userInfo)
-  function add123() {
-    dispatch(login({
-      phone: 15528022339,
-      password: '123333'
-    }))
-    // console.log(redux.userInfo)
-  }
-  function add345() {
-    dispatch(loginStatus())
-    // console.log(redux)
+  const searchValChange = (val) => {
+    console.log(val)
   }
   return (
     <View className='index'>
-      <Text>Hello world</Text>
-      <button onClick={add123}>333</button>
-      <button onClick={add345}>333</button>
+      <View className="header">
+        <View className="iconfont icon-voice"></View>
+        <AtInput
+          className="search-input"
+          name="search"
+          onChange={searchValChange}
+          placeholder="搜索音乐、歌词、电台"
+          placeholderClass="header-input-placeholder"></AtInput>
+        <View className="iconfont icon-musiclist"></View>
+      </View>
     </View>
     )
 }
