19e3eb2e7bad977e0fc4828e85d88331db858f69
Bacuuu <823727097@qq.com>
2021-12-16 20:49:28 +0800

    feat:歌单推荐页

--- src/pages/index/sheetTab.tsx
+++ src/pages/index/sheetTab.tsx
@@ -1,17 +1,18 @@
-import Taro, { useReady } from "@tarojs/taro"
+import Taro from '@tarojs/taro'
 import { getGreatSheet, getSheetTag } from "@/api/music"
-import { ScrollView, View } from "@tarojs/components"
+import { ScrollView, View, Text } from "@tarojs/components"
 import { usePageScroll } from "@tarojs/taro"
 import { useEffect, useState } from "react"
 import { useLazyLoad } from "@/hooks"
+import { numberFormatByZh } from '@/util'
+import { AtIcon } from "taro-ui"
 const sheetTab = function () {
   const [sheet, setSheet] = useState([])
   const [tags, setTags] = useState([])
-  // 抽离一个懒加载hook出去
+  const [isShowActionSheet, setShowActionSheet] = useState(false)
   usePageScroll((e) => {
     console.log(e)
   })
-  // todo
   useEffect(() => {
     getSheetTag()
       .then(r => {
@@ -19,26 +20,43 @@ const sheetTab = function () {
       })
     getGreatSheet({ limit: '20' })
       .then(r => {
-        setSheet(r.playLists)
+        setSheet(r.playlists)
       })
   }, [])
   let onScroll = useLazyLoad(() => {
     console.log('nice')
   }, 200, '.tab-content')
-  // useReady(() => {
-  //   const query = Taro.createSelectorQuery()
-  //   query.select('.tab-content')
-  //     .boundingClientRect()
-  //     .exec(res => {
-  //       console.log(res)
-  //     })
-  // })
-  // const onScroll = function (e) {
-  //   console.log(e.detail)
-  // }
+  const clickSheetItem = function () {
+    Taro.navigateTo({
+      url: '/pages/sheet/sheetList'
+    })
+  }
+  const showActionSheet = function () {
+    setShowActionSheet(true)
+  }
   return (
     <ScrollView className="tab-content" scrollY onScroll={onScroll}>
-
+      <View className="sheet-header">
+        <Text>全部歌单</Text>
+        <View className="sheet-header_icon">
+          <AtIcon size="20" value="bullet-list" onClick={showActionSheet}></AtIcon>
+        </View>
+      </View>
+      <View className="sheet-content">
+      {
+        sheet.map((i:any) => {
+          return (
+            <View className="sheet-item" onClick={clickSheetItem}>
+              <View className="sheet-item_bg" style={`background-image: url(${i.coverImgUrl})`}>
+                <Text className="sheet-item_hot"><AtIcon value='play' size='14' color='#FFF'></AtIcon>{numberFormatByZh(i.playCount)}</Text>
+                <Text className="sheet-item_tags">{i.tags.slice(0, 3).join(' | ')}</Text>
+              </View>
+              <Text className="sheet-item_title">{i.name}</Text>
+            </View>
+          )
+        })
+      }
+      </View>
     </ScrollView>
   )
 }
