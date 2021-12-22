71561525720e5bfc8ec0365ddd6be8dee53bb1fd
Bacuuu <823727097@qq.com>
2021-12-22 18:01:12 +0800

    feat:其他修改

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,9 +1,10 @@
 import { View, Text, ScrollView } from '@tarojs/components'
-import { AtInput, AtTabs, AtTabsPane, AtIcon } from 'taro-ui'
-import swiper from '../../components/index/swiper'
-import recommendSheet from '../../components/index/recommendSheet'
-import recommendNewSongs from '../../components/index/recommendNewSongs'
-import sheetTab from '../../components/sheet/sheetTab'
+import { AtInput, AtTabs, AtTabsPane, AtIcon  } from 'taro-ui'
+import { MyTabs, MyTabItem } from '@/components/common/Mytabs'
+import Swiper from '../../components/index/swiper'
+import RecommendSheet from '../../components/index/recommendSheet'
+import RecommendNewSongs from '../../components/index/recommendNewSongs'
+import SheetTab from '../../components/sheet/sheetTab'
 import './index.scss'
 import { useState } from 'react'
 
@@ -14,15 +15,7 @@ function Index (props: IProps) {
   const searchValChange = (val) => {
     console.log(val)
   }
-  const tabList = [{
-    title: '个性推荐'
-  }, {
-    title: '歌单'
-  }, {
-    title: '主播电台'
-  }, {
-    title: '排行榜'
-  }]
+  const tabList = ['个性推荐', '歌单', '主播电台', '排行榜']
   const [tabIdx, setTabIdx] = useState(0)
   return (
     <View className='index'>
@@ -36,10 +29,10 @@ function Index (props: IProps) {
           placeholderClass="header-input-placeholder"></AtInput>
         <View className="iconfont icon-musiclist"></View>
       </View>
-      <AtTabs className="tabs" current={tabIdx} tabList={tabList} onClick={setTabIdx}>
-        <AtTabsPane current={tabIdx} index={0} >
+      <MyTabs className="tabs" tabs={tabList}>
+        <MyTabItem index={0}>
           <ScrollView scrollY className="tab-content">
-            {swiper()}
+            <Swiper></Swiper>
             <View className="menu">
               <View className="menu-item">
                 <View className="menu-item_icon">
@@ -60,14 +53,14 @@ function Index (props: IProps) {
                 云音乐热歌榜
               </View>
             </View>
-            {recommendSheet()}
-            {recommendNewSongs()}
+            <RecommendSheet />
+            <RecommendNewSongs />
           </ScrollView>
-        </AtTabsPane>
-        <AtTabsPane current={tabIdx} index={1}>
-          {sheetTab()}
-        </AtTabsPane>
-      </AtTabs>
+        </MyTabItem>
+        <MyTabItem index={1}>
+          <SheetTab></SheetTab>
+        </MyTabItem>
+      </MyTabs>
     </View>
   )
 }
