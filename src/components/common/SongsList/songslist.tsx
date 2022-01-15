3b29c07cf8804c19d4549d764f22c283abda92e2
Bacuuu <823727097@qq.com>
2022-01-15 22:16:56 +0800

    feat:歌单页面

--- src/components/common/SongsList/songslist.tsx
+++ src/components/common/SongsList/songslist.tsx
@@ -1,16 +1,33 @@
-import { View } from "@tarojs/components"
+import { View, Text, Image } from "@tarojs/components"
+import { featureDelayMsg } from '@/util'
+import Taro from '@tarojs/taro'
+import { AtMessage } from "taro-ui"
 import './index.scss'
 const songslist = function (props: {list:Object[]}) {
-
+  const play = function () {
+    Taro.navigateTo({
+      url: '/pages/musicplay/musicPlay'
+    })
+  }
   return (
     <View className="list-wrap">
-      {props.list.map((i:any) => {
+      {props.list.map((i:any, index) => {
         return (
           <View className="list-item">
-            {i.name}
+            <View className="index">{index}</View>
+            <View className="item-right" onClick={play}>
+              <View className="info">
+                <Text className="name ellipsis">{i.name}</Text>
+                <Text className="from ellipsis">{i.from}</Text>
+              </View>
+              <View className="operation">
+                <Image className="mv" onClick={featureDelayMsg} src={require('@/assets/images/mv.png')}></Image>
+              </View>
+            </View>
           </View>
         )
       })}
+      <AtMessage></AtMessage>
     </View>
   )
 }
