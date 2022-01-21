0857cbba9605c92a4e051973a266c7e05d5842d6
Bacuuu <823727097@qq.com>
2022-01-21 17:53:20 +0800

    feat:初始化进行播放

--- src/components/common/PlayMenu/playMenu.tsx
+++ src/components/common/PlayMenu/playMenu.tsx
@@ -1,7 +1,21 @@
+import Taro from '@tarojs/taro'
 import { View, Text, Image } from '@tarojs/components'
+import { getPlayUrl } from '@/api/music'
 import { AtSlider } from 'taro-ui'
 import './index.scss'
-const playMenu = function () {
+import { useEffect } from 'react'
+const playMenu = function (props) {
+  const musicPlay = function (id) {
+    getPlayUrl({id}).then(r => {
+        Taro.playBackgroundAudio({
+          dataUrl: r.data[0].url
+        })
+      })
+  }
+  useEffect(() => {
+    if (!props.songId) return
+    musicPlay(props.songId)
+  }, [props.songId])
   return (
     <View className="playmenu-wrap">
       <View className="progress">
