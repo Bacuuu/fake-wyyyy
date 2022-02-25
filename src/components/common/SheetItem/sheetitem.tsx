b229272b5db37dcbb4f4dce30c15befd8cbc4203
Bacuuu <823727097@qq.com>
2022-02-25 22:02:15 +0800

    fix:完善我的音乐tab

--- src/components/common/SheetItem/sheetitem.tsx
+++ src/components/common/SheetItem/sheetitem.tsx
@@ -1,10 +1,11 @@
+import { Jumper } from "@/util"
 import { View, Image, Text } from "@tarojs/components"
 import styles from './index.module.scss'
 
 const sheetItem = function (props) {
   const { sheetInfo } = props
   return (
-    <View className={styles["sheet-item"]}>
+    <View className={styles["sheet-item"]} onClick={() => Jumper.toSheet(sheetInfo.id)}>
       <Image className={styles["sheet-image"]} src={sheetInfo.coverImgUrl}></Image>
       <View className={styles["sheet-info"]}>
         <Text className={styles["title"] + " ellipsis"}>{sheetInfo.name}</Text>
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,16 +1,16 @@
-import { View, Text } from '@tarojs/components'
+import { View, Text, Button } from '@tarojs/components'
 import { SheetItem } from '@/components/common/SheetItem'
 import styles from './home.module.scss'
 import { useEffect, useState } from 'react'
 import { getUserSheet } from '@/api/user'
 import { useSelector } from 'react-redux'
 import { IStoreType } from '@/types/store'
+import { Jumper } from '@/util'
 
 interface ISheetType {
   collected: Array<Object>,
   created: Array<Object>
 }
-
 function Home () {
   const user = useSelector((state:IStoreType) => state.user)
   const [sheetInfo, setSheetInfo] = useState({collected: [], created: []} as ISheetType)
@@ -32,20 +32,27 @@ function Home () {
     })
   }, [user.userInfo])
   return (
-    <View className={styles["home"]}>
-      <Text>我创建的歌单</Text>
-      {sheetInfo.created.map(i => {
-        return (
-          <SheetItem sheetInfo={i} showCreator={false}></SheetItem>
-        )
-      })}
-      <Text>我收藏的歌单</Text>
-      {sheetInfo.collected.map(i => {
-        return (
-          <SheetItem sheetInfo={i} showCreator></SheetItem>
-        )
-      })}
-    </View>
+    user.userInfo
+    ? (
+      <View className={styles["home"]}>
+        <Text className={styles["block-title"]}>我创建的歌单({sheetInfo.created.length}首)</Text>
+        {sheetInfo.created.map(i => {
+          return (
+            <SheetItem sheetInfo={i} showCreator={false}></SheetItem>
+          )
+        })}
+        <Text className={styles["block-title"]}>我收藏的歌单({sheetInfo.collected.length}首)</Text>
+        {sheetInfo.collected.map(i => {
+          return (
+            <SheetItem sheetInfo={i} showCreator></SheetItem>
+          )
+        })}
+      </View>
+    ) : (
+      <View className={styles["to-login"]}>
+        <Button className={styles["btn"]} onClick={Jumper.toLogin}>去登陆</Button>
+      </View>
     )
+  )
 }
 export default Home
